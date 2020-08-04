7e09b948cd9b27fd006c640ec56196c934565ef0
Nelma da Silva Barros <nelmasb@gmail.com>
2020-08-03 22:35:27 -0300

    App de estudo cutso udemy - aprenda a falar em inglês app#6

---
+++ app/src/main/java/com/example/aprenda/ingles/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.aprenda.ingles;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.example.aprenda.ingles.fragments.BichosFragment;
+import com.example.aprenda.ingles.fragments.NumerosFragment;
+import com.example.aprenda.ingles.fragments.VogaisFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //removendo sombra na actionbar
+        getSupportActionBar().setElevation(0);
+
+        smartTabLayout = findViewById(R.id.viewPagerTab);
+        viewPager = findViewById(R.id.viewPager);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("Bichos", BichosFragment.class)
+                        .add("Numeros", NumerosFragment.class)
+                        .add("Vogais", VogaisFragment.class)
+                        .create()
+        );
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+    }
+}
